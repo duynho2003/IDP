2015eb2fe664d056422b79fa7d8c70876668761d
Tommy Nguyen <khenchaynguyen@gmail.com>
2024-05-22 18:52:26 +0700

    up

--- InheLab01 - Answer/Student.dart
+++ InheLab01 - Answer/Student.dart
@@ -8,7 +8,7 @@ class Student extends Person {
   String email = '';
 
   Student();
-  
+
   Student.full(String name, String gender, DateTime birthDate, String address,
       this.studentId, this.gpa, this.email)
       : super.full(name, gender, birthDate, address);
@@ -17,23 +17,23 @@ class Student extends Person {
   void inputInfo() {
     super.inputInfo();
 
-    print('Enter student ID (8 characters):');
-    studentId = stdin.readLineSync() ?? '';
+    // print('Enter student ID (8 characters):');
+    // studentId = stdin.readLineSync() ?? '';
     do {
       print('Enter student ID (8 characters):');
       studentId = stdin.readLineSync() ?? '';
-    } while (studentId.length >= 8);
+    } while (studentId.length != 8);
 
-    print('Enter GPA (0.0 - 10.0):');
-    gpa = double.parse(stdin.readLineSync() ?? '');
-     do {
+    // print('Enter GPA (0.0 - 10.0):');
+    // gpa = double.parse(stdin.readLineSync() ?? '');
+    do {
       print('Enter GPA (0.0 - 10.0):');
       gpa = double.parse(stdin.readLineSync() ?? '');
     } while (gpa < 0.0 || gpa > 10.0);
 
-    print('Enter email (must contain @):');
-    email = stdin.readLineSync() ?? '';
-     do {
+    // print('Enter email (must contain @):');
+    // email = stdin.readLineSync() ?? '';
+    do {
       print('Enter email (must contain @):');
       email = stdin.readLineSync() ?? '';
     } while (!email.contains('@'));
@@ -50,6 +50,6 @@ class Student extends Person {
   }
 
   bool isScholarshipEligible() {
-    return gpa > 8.0;
+    return gpa >= 8.0;
   }
 }
